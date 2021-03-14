46709ff70412e65d7e0c263b0358d234d75cdca2
Tabalbar <Tabalbar@hawaii.edu>
2021-03-13 23:58:33 -1000

    set up graph vis

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,18 +1,66 @@
 import React, {useEffect, useState} from 'react'
 import openSocket from 'socket.io-client'
+import Graph from 'react-graph-vis'
+import { T } from 'lodash/fp'
 
 var socket = openSocket('http://localhost:5000', {transports: ['websocket']})
 
 function App() {
 
 const [tweets, setTweets] = useState([])
+const [graph, setGraph] = useState({
+  nodes: [],
+  edges: []
+})
+const [count, setCount] = useState(0)
+const [secondCount, setSecondCount] = useState(1000)
 
   useEffect(() => {
     socket.on('latest tweets', mapTweetsToState)
   },[])
-
+console.log(graph)
   const mapTweetsToState = (tweet) => {
     setTweets(prev=>[...prev, tweet])
+    const node = {
+      id: count,
+      label: tweet.user
+    }
+    if(tweet.retweetedPerson.name === '' ){
+      let tmpGraph = graph
+      tmpGraph.nodes.push(node)
+      setGraph(tmpGraph)
+
+    } else {
+      let found = false;
+      let index = 0
+      for(let i = 0; i < graph.nodes.length; i++){
+        if(graph.nodes[i].label === tweet.retweetedPerson.user){
+          found = true;
+          index = i
+          break;
+        }
+      }
+      if(found) {
+        let tmpGraph = graph
+        tmpGraph.nodes.push(node)
+        tmpGraph.edges.push({from: count, to: index})
+        setGraph(tmpGraph)
+      } else {
+        let tmpGraph = graph
+        tmpGraph.nodes.push(node)
+        // setGraph(tmpGraph)
+        let newNode = {
+          id: secondCount,
+          label: tweet.retweetedPerson.user
+        }
+        tmpGraph.nodes.push(newNode)
+        tmpGraph.edges.push({from: count, to: secondCount})
+        setGraph(tmpGraph)
+        setSecondCount(prev=>prev+1)
+      }
+    }
+    setCount(prev=>prev+1)
+
   }
 
   const stopStreaming = () => {
@@ -22,7 +70,20 @@ const [tweets, setTweets] = useState([])
 const startStreaming = () => {
   socket.emit('start stream', () => { });
 }
-
+const options = {
+  layout: {
+    hierarchical: true
+  },
+  edges: {
+    color: "#000000"
+  },
+  height: "500px"
+};
+const events = {
+  select: function(event) {
+    var { nodes, edges } = event;
+  }
+};
   // const callApi = async () => {
   //   const response = await fetch('http://localhost:5000/api/hello');
   //   const body = await response.json();
@@ -39,13 +100,21 @@ const startStreaming = () => {
           {tweets.map((tweet) => {
             return (
               <div>
-                <p>{tweet.user}</p>
+                {/* <p>{tweet.user} {tweet.retweetedPerson.name === ''? null:'retweeted'} {tweet.retweetedPerson.name}</p>
                 <p>{tweet.text}</p>
-                <p>************************</p>
+                <p>************************</p> */}
               </div>
             )
           })}
         </div>
+        <Graph
+      graph={graph}
+      options={options}
+      events={events}
+      getNetwork={network => {
+        //  if you want access to vis.js network api you can set the state in a parent component using this property
+      }}
+    />
     </>
   );
 }
--- tweets.js
+++ tweets.js
@@ -5,23 +5,31 @@ var T = new twit({
     consumer_secret: 'WtCfgzycOzNeXceNR42hCPqb8A1Bb5rVv0KwviFaHUivFB6gc6',
     access_token: '1161632035-VryYFCfEsxybe3KK8nAztx4FGXz2I9JNIg0bvEZ',
     access_token_secret: 'qWqq882FT5H6clsRX43f0coBc6s5Bu17yOybNIeqndONV',
-    timeout_ms: 60*1000,
+    timeout_ms: 60 * 1000,
     strictSSL: true,
 });
+var hawaiiHonoulu = ['-170.97', '12.09', '-142.68', '33.96']
+// let stream = T.stream('statuses/filter', {locations: hawaiiHonoulu})
+let stream = T.stream('statuses/filter', { track: 'food' })
 
-let stream = T.stream('statuses/filter', {track: 'Japan'})
 let isStreamStopped = false;
 function getTweetObject(tweet) {
     let tweetText = (tweet.extended_tweet) ? tweet.extended_tweet.full_text : tweet.text;
-
+    let retweetedPerson = {id: '', name: ''}
     // check for retweets
     if (tweet.text.includes('RT @') && tweet.retweeted_status) {
         tweetText = (tweet.retweeted_status.extended_tweet) ? tweet.retweeted_status.extended_tweet.full_text : tweet.retweeted_status.text;
-    }
+        retweetedPerson.name = (tweet.retweeted_status) ? tweet.retweeted_status.user.name : ''
+        retweetedPerson.id = (tweet.retweeted_status) ? tweet.retweeted_status.user.id : ''
 
+    }
+    console.log(retweetedPerson)
+    tweetText = tweet.text
     let TweetObject = {
         text: tweetText,
+        id: tweet.id,
         user: tweet.user.name,
+        retweetedPerson: retweetedPerson,
         location: (tweet.user.location !== null) ? tweet.user.location : '',
         followers: tweet.user.followers_count,
         userImage: tweet.user.profile_image_url,
@@ -50,6 +58,7 @@ module.exports = (io) => {
 
             stream.on('tweet', function (tweet) {
                 console.log('tweeting');
+                // console.log(tweet)
 
                 let TweetObject = getTweetObject(tweet);
                 socket.emit('latest tweets', TweetObject);
@@ -57,5 +66,5 @@ module.exports = (io) => {
             stream.start();
             isStreamStopped = false;
         })
-    }) 
+    })
 }
